4070115a22ba2495f4480958c29884b387becead
John Lee <john@expensify.com>
2025-01-29 13:17:00 -0500

    Safe access of task titles

--- src/CONST.ts
+++ src/CONST.ts
@@ -1281,6 +1281,7 @@ const CONST = {
         OWNER_EMAIL_FAKE: '__FAKE__',
         OWNER_ACCOUNT_ID_FAKE: 0,
         DEFAULT_REPORT_NAME: 'Chat Report',
+        DEFAULT_REPORT_ID: '-1',
         PERMISSIONS: {
             READ: 'read',
             WRITE: 'write',
